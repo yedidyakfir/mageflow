f29a7d35225105749630031fdb4bff4a068e4764
yedidyakfir <yedidyakfir@gmail.com>
2025-11-12 16:54:48 +0200

    move creator for signature

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -1,48 +1,13 @@
-from datetime import datetime
-from typing import TypedDict, overload, Any
-
 from orchestrator.callbacks import register_task, handle_task_callback
 from orchestrator.chain.creator import chain
 from orchestrator.client import Orchestrator
 from orchestrator.init import init_orchestrator_hatchet_tasks
+from orchestrator.signature.creator import sign
 from orchestrator.signature.model import TaskSignature
 from orchestrator.signature.status import TaskStatus
 from orchestrator.signature.types import HatchetTaskType, TaskIdentifierType
 from orchestrator.startup import init_from_dynaconf
 from orchestrator.swarm.creator import swarm
-
-try:
-    # Python 3.12+
-    from typing import NotRequired, Unpack
-except ImportError:
-    # Older Python versions
-    from typing_extensions import NotRequired, Unpack
-
-
-class TaskSignatureOptions(TypedDict, total=False):
-    kwargs: dict
-    workflow_params: dict
-    creation_time: datetime
-    model_validators: Any
-    success_callbacks: list[TaskIdentifierType]
-    error_callbacks: list[TaskIdentifierType]
-    task_status: TaskStatus
-    task_identifiers: dict
-
-
-@overload
-async def sign(
-    task: str | HatchetTaskType, **options: Unpack[TaskSignatureOptions]
-) -> TaskSignature: ...
-@overload
-async def sign(task: str | HatchetTaskType, **options: Any) -> TaskSignature: ...
-
-
-async def sign(task: str | HatchetTaskType, **options: Any) -> TaskSignature:
-    if isinstance(task, str):
-        return await TaskSignature.from_task_name(task, **options)
-    else:
-        return await TaskSignature.from_task(task, **options)
 
 
 load_signature = TaskSignature.from_id_safe
--- orchestrator/signature/creator.py
+++ orchestrator/signature/creator.py
@@ -1,10 +1,12 @@
-from typing import TypeAlias
+from datetime import datetime
+from typing import TypeAlias, TypedDict, Any, overload
 
 from orchestrator.signature.model import (
     TaskSignature,
     TaskIdentifierType,
     HatchetTaskType,
 )
+from orchestrator.signature.status import TaskStatus
 
 TaskSignatureConvertible: TypeAlias = (
     TaskIdentifierType | TaskSignature | HatchetTaskType
@@ -18,3 +20,37 @@ async def resolve_signature_id(task: TaskSignatureConvertible) -> TaskSignature:
         return await TaskSignature.from_id_safe(task)
     else:
         return await TaskSignature.from_task(task)
+
+
+try:
+    # Python 3.12+
+    from typing import Unpack
+except ImportError:
+    # Older Python versions
+    from typing_extensions import Unpack
+
+
+class TaskSignatureOptions(TypedDict, total=False):
+    kwargs: dict
+    workflow_params: dict
+    creation_time: datetime
+    model_validators: Any
+    success_callbacks: list[TaskIdentifierType]
+    error_callbacks: list[TaskIdentifierType]
+    task_status: TaskStatus
+    task_identifiers: dict
+
+
+@overload
+async def sign(
+    task: str | HatchetTaskType, **options: Unpack[TaskSignatureOptions]
+) -> TaskSignature: ...
+@overload
+async def sign(task: str | HatchetTaskType, **options: Any) -> TaskSignature: ...
+
+
+async def sign(task: str | HatchetTaskType, **options: Any) -> TaskSignature:
+    if isinstance(task, str):
+        return await TaskSignature.from_task_name(task, **options)
+    else:
+        return await TaskSignature.from_task(task, **options)
