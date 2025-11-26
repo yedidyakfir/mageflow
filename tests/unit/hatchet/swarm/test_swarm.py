49ff1568cd0934261c514cae1fbc4963d2c94729
yedidyakfir <yedidyakfir@gmail.com>
2025-11-26 13:19:23 +0200

    move the test

--- tests/unit/hatchet/swarm/test_swarm.py
+++ tests/unit/hatchet/swarm/test_swarm.py
@@ -1,7 +1,5 @@
 import pytest
 
-import orchestrator
-from orchestrator.errors import TooManyTasksError
 from orchestrator.signature.model import TaskSignature
 from orchestrator.swarm.model import SwarmTaskSignature, SwarmConfig
 from tests.integration.hatchet.models import ContextMessage
@@ -134,21 +132,3 @@ async def test_add_to_running_tasks_sanity(
             == new_tasks_left
             == reloaded_swarm.tasks_left_to_run
         )
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(["max_task_allowed"], [[2], [1], [5]])
-async def test_add_task_exceeds_max_task_allowed_error(max_task_allowed):
-    # Arrange
-    swarm_signature = await orchestrator.swarm(
-        task_name="test_swarm",
-        tasks=[
-            TaskSignature(task_name=f"test_task_{i}") for i in range(max_task_allowed)
-        ],
-        model_validators=ContextMessage,
-        config=SwarmConfig(max_task_allowed=max_task_allowed),
-    )
-
-    # Act & Assert
-    with pytest.raises(TooManyTasksError):
-        await swarm_signature.add_task(TaskSignature(task_name="test_task_last"))
