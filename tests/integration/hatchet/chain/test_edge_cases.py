1866fad32554c87514e9a6db533040569651e0d0
YedidyaHKfir <Yedidya@dono.ai>
2025-12-11 18:24:34 +0200

    added tests for timeout task for chain and signature

--- tests/integration/hatchet/chain/test_edge_cases.py
+++ tests/integration/hatchet/chain/test_edge_cases.py
@@ -1,6 +1,49 @@
+import asyncio
+
 import pytest
+
+import mageflow
+from tests.integration.hatchet.assertions import (
+    get_runs,
+    assert_signature_done,
+    assert_signature_not_called,
+)
+from tests.integration.hatchet.conftest import HatchetInitData
+from tests.integration.hatchet.models import ContextMessage
+from tests.integration.hatchet.worker import (
+    timeout_task,
+    error_callback,
+    chain_callback,
+)
 
 
 @pytest.mark.asyncio(loop_scope="session")
-async def test__chain__task_timeout__chain_call_error_callback():
-    pass
+async def test__chain__task_timeout__chain_call_error_callback(
+    hatchet_client_init: HatchetInitData,
+    test_ctx,
+    ctx_metadata,
+    trigger_options,
+    sign_task1,
+):
+    # Arrange
+    redis_client, hatchet = (
+        hatchet_client_init.redis_client,
+        hatchet_client_init.hatchet,
+    )
+    success_callback_sign = await mageflow.sign(chain_callback)
+    error_callback_sign = await mageflow.sign(error_callback)
+    chain_signature = await mageflow.chain(
+        [sign_task1, timeout_task],
+        success=success_callback_sign,
+        error=error_callback_sign,
+    )
+
+    # Act
+    await chain_signature.aio_run_no_wait(ContextMessage(), options=trigger_options)
+    await asyncio.sleep(15)
+
+    # Assert
+    runs = await get_runs(hatchet, ctx_metadata)
+
+    assert_signature_done(runs, error_callback_sign)
+    assert_signature_not_called(runs, success_callback_sign)
--- tests/integration/hatchet/signature/test_edge_case.py
+++ tests/integration/hatchet/signature/test_edge_case.py
@@ -1,6 +1,31 @@
+import asyncio
+
 import pytest
+
+import mageflow
+from tests.integration.hatchet.assertions import get_runs, assert_signature_done
+from tests.integration.hatchet.conftest import HatchetInitData
+from tests.integration.hatchet.models import ContextMessage
+from tests.integration.hatchet.worker import timeout_task, error_callback
 
 
 @pytest.mark.asyncio(loop_scope="session")
-async def test__timeout_task__call_error_callback():
-    pass
+async def test__timeout_task__call_error_callback(
+    hatchet_client_init: HatchetInitData, test_ctx, ctx_metadata, trigger_options
+):
+    # Arrange
+    redis_client, hatchet = (
+        hatchet_client_init.redis_client,
+        hatchet_client_init.hatchet,
+    )
+    error_sign = await mageflow.sign(error_callback)
+    timeout_sign = await mageflow.sign(timeout_task, error_callbacks=[error_sign])
+    message = ContextMessage(base_data=test_ctx)
+
+    # Act
+    await timeout_sign.aio_run_no_wait(message)
+    await asyncio.sleep(10)
+
+    # Assert
+    runs = await get_runs(hatchet, ctx_metadata)
+    assert_signature_done(runs, error_sign, **message.model_dump(mode="json"))
